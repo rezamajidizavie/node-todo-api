cb5a98f7a1050150432e8e4107bbd0e595ab4013
Reza Majidi <rezamajidizavie@gmail.com>
2018-09-19 21:02:12 +0430

    m

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,6 +8,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID("5ba26d46c7f8782becbada5a")
+    }, {
+        $set: {
+            name: 'Ahmad'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result)
+    })
 
 
 });
