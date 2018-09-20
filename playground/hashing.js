095f4a95f11969bf75ec90ddb5aca3cb382695ad
Reza Majidi <rezamajidizavie@gmail.com>
2018-09-20 13:56:04 +0430

    m

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,42 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-let message = 'I am user number 3';
+let data = {
+    id: 10
+};
 
-let hash = SHA256(message).toString();
+let token = jwt.sign(data, '123abc');
 
-console.log(message, hash);
+console.log(token);
 
+let decoded = jwt.verify(token, '123abc');
+
+console.log(decoded);
+
+
+// let message = 'I am user number 3';
+//
+// let hash = SHA256(message).toString();
+//
+// console.log(message, hash);
+//
+// let data = {
+//     id: 4
+// };
+//
+// let token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'someSecret').toString()
+// };
+//
+// //
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// let resultHash = SHA256(JSON.stringify(token.data) + 'someSecret').toString();
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed')
+// } else {
+//     console.log('Data was changed, dont trust')
+// }
+//
